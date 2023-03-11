0f10d0167dbe3f194994823f2762448bbcb92b51
alex belost <alex.belost@gmail.com>
2023-03-11 08:39:13 +0200

    Create base functionality for the Logger
    Fix errors
    
    Fix errors
    
    fix errors
    
    Fixes
    
    Fix errors
    
    fixes

---
+++ lua/logger/init.lua
@@ -0,0 +1,28 @@
+local M = {}
+
+function M.logger_run()
+  -- check if current file is a JavaScript or TypeScript file
+  local filetype = vim.bo.filetype
+  if filetype ~= "javascript" and filetype ~= "typescript" then
+    print("This command is only available for JavaScript and TypeScript files")
+    return
+  end
+
+  local line = vim.fn.line('.')
+  local col = vim.fn.col('.')
+  local full_buffer_name = vim.api.nvim_buf_get_name(0)
+  local buffer_name = vim.fn.fnamemodify(full_buffer_name, ":t")
+  local word = vim.fn.expand("<cword>")
+
+  -- build the console log statement
+  local console_log = "console.log('🛠  " ..
+      line .. ":" .. col .. " " .. buffer_name .. " -> " .. word .. ": ', " .. word .. ");"
+
+  -- go to the end of the current line
+  vim.api.nvim_command("normal! $")
+
+  -- insert a new line and the console log statement
+  vim.api.nvim_command("normal! o" .. console_log)
+end
+
+return M
---
+++ plugin/logger.vim
@@ -0,0 +1,1 @@
+command! -nargs=* LoggerRun lua require("logger").logger_run()
